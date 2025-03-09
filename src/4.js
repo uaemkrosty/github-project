516ec40e2991c676fa20a7bc88810203ad5fb649
uaemkrosty <uaemkrosty@gmail.com>
2025-03-09 16:05:59 +0000

    Add 4.js

---
+++ src/4.js
@@ -0,0 +1,8 @@
+// Generate a random number between 1 and 10
+function getRandomNumber() {
+  return Math.floor(Math.random() * 10) + 1;
+}
+
+// Use the function to generate a random number
+let myNumber = getRandomNumber();
+console.log(myNumber);
