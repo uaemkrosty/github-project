d90c63e5380ef6b9bc568aa9ad8b198e56f9d2c9
uaemkrosty <uaemkrosty@gmail.com>
2025-03-11 15:44:22 +0000

    Add 6.js

---
+++ src/6.js
@@ -0,0 +1,9 @@
+
+function getRandomCode() {
+  var result = '';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (var i = 0; i < 10; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
