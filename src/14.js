0bbc0c45e98ffbccba150a3e5a77c80d5331b3af
uaemkrosty <uaemkrosty@gmail.com>
2025-03-20 12:23:04 +0000

    Add 14.js

---
+++ src/14.js
@@ -0,0 +1,7 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+// Example usage
+console.log(getRandomInt(10)); // Output: 0
+console.log(getRandomInt(20)); // Output: 7
