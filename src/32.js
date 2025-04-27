a29e5d754c591d1e109b44a248afa0a46eaf0b36
uaemkrosty <uaemkrosty@gmail.com>
2025-04-27 01:28:35 +0000

    Add 32.js

---
+++ src/32.js
@@ -0,0 +1,12 @@
+function addTwoNumbers(x, y) {
+  return x + y;
+}
+
+function displayNumber(num) {
+  console.log(num);
+}
+
+// Example usage:
+let result = addTwoNumbers(5, 3); // Output: 8
+
+displayNumber(result);
